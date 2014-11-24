2c567a614c7872ba1fb2b5db20e0e737803a8653
Thomas Fisher-York <tly2@cornell.edu>
2014-11-23 22:47:23 -0500

    renamed a variable

--- bin/h.pl
+++ bin/h.pl
@@ -18,46 +18,46 @@ my $data_obj = Data->new( { data_source_filename => $input_data_filename } );
 
 my $sum_tree = Tree->new();
 #exit;
-my $tree_obj =MCTree->new( { data => $data_obj, N => $data_obj->N(), repeatable => $repeatable, sum_tree => $sum_tree }); #$repeatable } );
-print "# ", $tree_obj->info_string(), "\n";
+my $mc_tree =MCTree->new( { data => $data_obj, N => $data_obj->N(), repeatable => $repeatable, sum_tree => $sum_tree }); #$repeatable } );
+print "# ", $mc_tree->info_string(), "\n";
 
 #exit;
 
-$tree_obj->root()->split_node_mcmc(1e-10);
+$mc_tree->root()->split_node_mcmc(1e-10);
 
-print "# ", $tree_obj->info_string(), "\n";
+print "# ", $mc_tree->info_string(), "\n";
 
-my @leaf_ukeys = keys %{$tree_obj->leaf_unikeys()};
+my @leaf_ukeys = keys %{$mc_tree->leaf_unikeys()};
 print STDERR "# ", join(", ", @leaf_ukeys), "\n";
 for my $a_leaf_unikey (@leaf_ukeys){
-  my $leaf_node = $tree_obj->unikey_node()->{$a_leaf_unikey};
+  my $leaf_node = $mc_tree->unikey_node()->{$a_leaf_unikey};
   $leaf_node->split_node_mcmc(1e-10);
 }
-print "# ", $tree_obj->info_string(), "\n";
+print "# ", $mc_tree->info_string(), "\n";
 
-print "# ", $tree_obj->newick(), "\n";
-die "check failed after splitting.\n" if(! $tree_obj->check());
+print "# ", $mc_tree->newick(), "\n";
+die "check failed after splitting.\n" if(! $mc_tree->check());
 
 
 for my $a_leaf_unikey (@leaf_ukeys) {
-  my $leaf_node = $tree_obj->unikey_node()->{$a_leaf_unikey};
+  my $leaf_node = $mc_tree->unikey_node()->{$a_leaf_unikey};
   $leaf_node->join_nodes_mcmc(1e10);
 }
-print "# ", $tree_obj->info_string(), "\n";
+print "# ", $mc_tree->info_string(), "\n";
 
-print "# ", $tree_obj->newick(), "\n";
-die "check failed after joining.\n" if(! $tree_obj->check());
+print "# ", $mc_tree->newick(), "\n";
+die "check failed after joining.\n" if(! $mc_tree->check());
 
 for (1..$n_mcmc_steps) {
   # print STDERR "mcmc split: $_\n";
-  $tree_obj->mcmc_step_split();
-  print $tree_obj->n_leaves(), "  ", $tree_obj->newick(), "\n";
- # print "n_leaves: ", $tree_obj->n_leaves(), "\n";
+  $mc_tree->mcmc_step_split();
+  print $mc_tree->n_leaves(), "  ", $mc_tree->newick(), "\n";
+ # print "n_leaves: ", $mc_tree->n_leaves(), "\n";
 # print STDERR "mcmc join: $_\n";
-  $tree_obj->mcmc_step_join();
-  print $tree_obj->n_leaves(), "  ", $tree_obj->newick(), "\n";
-  # print $tree_obj->newick(), "\n";
- # print "n_leaves: ", $tree_obj->n_leaves(), "\n";
+  $mc_tree->mcmc_step_join();
+  print $mc_tree->n_leaves(), "  ", $mc_tree->newick(), "\n";
+  # print $mc_tree->newick(), "\n";
+ # print "n_leaves: ", $mc_tree->n_leaves(), "\n";
 }
 
 
